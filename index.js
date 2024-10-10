a05287e0741980077ccad65d525f61c9883d7326
Debrah Muinde <debbynav645@gmail.com>
2024-10-10 06:53:00 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+//Multiply two numbers to get 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+//Generating a random integer greater than 0;
+let random = Math.ceil(Math.random() * 100);
+
+//Calculating the remainder
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+
+//Finding the highest number in the set
+let max = Math.max(5, 10, 15, 20);
